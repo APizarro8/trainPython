d899df6f83cbfdd38f7f256f9424bc5de32a9362
Alicia Pizarro <43381268+APizarro8@users.noreply.github.com>
2019-04-17 11:34:55 +0200

    Update Tortugas.py

--- Tortugas.py
+++ Tortugas.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 #Ejercicio 2 
-#Porp�sito: Probar el gráfico Tortuga. 
+#Porpósito: Probar el gráfico Tortuga. 
 #Autor: Alicia Pizarro
 #Fecha: 13/10/2017
 
+##Importación de módulos
 from turtle import Screen, Turtle
 
-pantalla = Screen() #Creaci�n de la pantalla y asignaci�n de la variable
-pantalla.setup (425, 225) #M�todo setup que invoca el ancho y alto de la variable
-pantalla.screensize (400, 200) # M�todo que fija el tama�o de de la superficie de dibujo
+#####################################################################################################
+pantalla = Screen() #Creación de la pantalla y asignación de la variable.
+pantalla.setup (425, 225) #Método setup que invoca el ancho y alto de la variable.
+pantalla.screensize (400, 200) # Método que fija el tamño de de la superficie de dibujo.
 
-tortuga = Turtle () #Creaci�n de la tortuga y asignaci�n de la variable 
-tortuga.forward (100) # M�todo que le indica a la tortuga que avance 100 pasos hacia la derecha
-tortuga.left (90) #M�todo que le indica a la tortuga que avance 90 pasos a la izquierda
+tortuga = Turtle () #Creación de la tortuga y asignación de la variable. 
+tortuga.forward (100) # Método que le indica a la tortuga que avance 100 pasos hacia la derecha.
+tortuga.left (90) #Método que le indica a la tortuga que avance 90 pasos a la izquierda.
 tortuga.forward (100)
 tortuga.left(90)
 tortuga.forward(100)
@@ -24,4 +26,6 @@ tortuga.circle (20)
 
 tortuga.st()
 
-pantalla.exitonclick() # M�todo para que la ventana creada no desaparezca
+pantalla.exitonclick() # Método para que la ventana creada no desaparezca
+
+#####################################################################################################
