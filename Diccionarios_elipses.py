63741e6a133944dc8c98a912d957517fcb251e06
Alicia Pizarro <43381268+APizarro8@users.noreply.github.com>
2019-04-23 15:17:10 +0200

    Update Diccionarios_elipses.py

--- Diccionarios_elipses.py
+++ Diccionarios_elipses.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 #Ejercicio 7 
-#Propósito: Realizar un programa que un menú que indique al usuario las diferentes opciones los parámetros del elipsoide correspondiente. 
+#Propósito: Realizar un programa que un menú que indique al usuario las diferentes opciones los parámetros del 
+#elipsoide correspondiente. 
 #Autor: Alicia Pizarro
 #Fecha: 15/11/2017
 
+#IMPORTACIÓN DE MÓDULOS
 import math
 
-
+##DEFINICIÓN DE DICCIONARIOS 
 def calculo_elipsoide (elipsoide,**datum):
     
     diccionario_result = datum
@@ -17,39 +19,34 @@ def calculo_elipsoide (elipsoide,**datum):
         diccionario_result['Excentricidad_primera'] = diccionario_result['Excentricidad_lineal']/diccionario_result['Semieje_mayor'] 
         diccionario_result['Excentricidad_segunda'] = diccionario_result['Excentricidad_lineal']/diccionario_result['Semieje_menor'] 
        
-    
     elif elipsoide == 2:
         diccionario_result['Excentricidad_lineal'] =  diccionario_result['Excentricidad_primera'] *  diccionario_result['Semieje_mayor'] 
         diccionario_result['Semieje_menor'] = math.sqrt( diccionario_result['Semieje_mayor']**2 -  diccionario_result['Excentricidad_lineal']**2)
         diccionario_result['Excentricidad_segunda'] =  diccionario_result['Excentricidad_lineal'] /  diccionario_result['Semieje_menor']
         diccionario_result['Aplanamiento'] = ( diccionario_result['Semieje_mayor'] -  diccionario_result['Semieje_menor'])/  diccionario_result['Semieje_mayor']
        
-        
     elif elipsoide == 3:
         diccionario_result['Semieje_mayor'] =  diccionario_result['Semieje_menor']/ (1-  diccionario_result['Aplanamiento'])
         diccionario_result['Excentricidad_lineal'] = math.sqrt( diccionario_result['Semieje_mayor']**2 -  diccionario_result['Semieje_menor']**2)
         diccionario_result['Excentricidad_primera'] =  diccionario_result['Excentricidad_lineal']/ diccionario_result['Semieje_mayor'] 
         diccionario_result['Excentricidad_segunda'] =  diccionario_result['Excentricidad_lineal']/ diccionario_result['Semieje_menor']
         
-    
     elif elipsoide == 4:
         diccionario_result['Semieje_mayor'] =  diccionario_result['Excentricidad_lineal'] /  diccionario_result['Excentricidad_primera']
         diccionario_result['Semieje_menor'] = math.sqrt(diccionario_result['Semieje_mayor']**2 -  diccionario_result['Excentricidad_lineal']**2)
         diccionario_result['Excentricidad_segunda'] =  diccionario_result['Excentricidad_lineal']/ diccionario_result['Semieje_menor']
         diccionario_result['Aplanamiento'] = ( diccionario_result['Semieje_mayor'] -  diccionario_result['Semieje_menor'])/  diccionario_result['Semieje_mayor']
        
-    
     elif elipsoide == 5:
         diccionario_result['Excentricidad_lineal'] = math.sqrt( diccionario_result['Semieje_mayor']**2- diccionario_result['Semieje_menor']**2)
         diccionario_result['Excentricidad_primera'] =  diccionario_result['Excentricidad_lineal']/ diccionario_result['Semieje_mayor']
         diccionario_result['Excentricidad_segunda'] =  diccionario_result['Excentricidad_lineal']/ diccionario_result['Semieje_menor']
         diccionario_result['Aplanamiento'] = ( diccionario_result['Semieje_mayor'] -  diccionario_result['Semieje_menor'])/  diccionario_result['Semieje_mayor']
        
-        
     for clave in datum:
         print clave, 'tiene un valor de ', datum[clave]
     
-
+##DEFINICIÓN DE PREGUNTAS Y ASIGNACIÓN DE RESPUESTAS 
 print("¿Qué elipsoide desea?")
 print("1.Hayford")
 print("2.NAD83")
@@ -76,4 +73,5 @@ while elipsoide > 0 and elipsoide <= 5:
 
     elipsoide = int(input("Introduce el elipsoide (1/2/3/4/5): "))
 
-        
+############################################################ FINISH RUN #####################################################################
+############################################################################################################################################
